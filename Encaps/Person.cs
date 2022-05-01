108bf361e7d6b1d4aa56b54dca86f954c9b70cd8
catalinacris <catalinacucos14@yahoo.com>
2022-05-01 05:47:08 +0200

    Add project files.

---
+++ Encaps/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encaps
+{
+    class Person
+    {
+        private readonly string cnp;
+        private string firstName;
+        private string lastName;
+        private decimal salary;
+
+        public Person(string cnp, string firstName, string lastName)
+        {
+            this.cnp = cnp;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.salary = 1000;
+        }
+
+        public string Cnp
+        {
+            get { return cnp; }  //public string Cnp {get => cnp; }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+        }
+
+        public string FullName
+        {
+            get { return firstName + " " + lastName; }
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+        }
+
+        private void IncreaseSalary()
+        {
+            this.salary = this.salary * (decimal)1.3;
+        }
+
+        public void GetMarried(string newLastName)
+        {
+            this.lastName = newLastName;
+            this.IncreaseSalary();
+            Console.WriteLine("Congrats, you got a bonus!");
+        }
+    }
+}
---
+++ Encaps/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Encaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("244444", "Maria", "Popescu");
+            Console.WriteLine(person.Cnp);
+            Console.WriteLine(person.FirstName);
+            Console.WriteLine(person.LastName);
+            Console.WriteLine(person.Salary);
+
+            person.GetMarried("Ionescu");
+            Console.WriteLine(person.LastName);
+            Console.WriteLine(person.FullName);
+            Console.WriteLine(person.Salary);
+
+
+        }
+    }
+}
